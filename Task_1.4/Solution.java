4f0b76ef753b6d5fecdd4d3c3c4552ec52bff6e0
Konstantin-Plastinin <70012289+Konstantin-Plastinin@users.noreply.github.com>
2020-08-23 10:52:15 +0300

    Create Solution.java

---
+++ Task_1.4/Solution.java
@@ -0,0 +1,11 @@
+/* Дана переменная number.
+Напиши программу, которая выводит на экран квадрат этой переменной (number * number). */
+
+public class Solution {
+    public static int number = 25;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(number * number);
+    }
+}
